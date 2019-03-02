86ba01822a78af1b05dabb2d16672ed7b77602d8
Ben Gubler <nebrelbug@gmail.com>
2019-03-02 11:09:04 -0700

    Re-enabled sourcemaps for squirrelly.min.js

--- rollup.config.js
+++ rollup.config.js
@@ -1,50 +1,50 @@
-import { uglify } from "rollup-plugin-uglify"
+import { uglify } from 'rollup-plugin-uglify'
 
 // rollup.config.js (building more than one bundle)
 
 export default [{
-    input: 'src/index.js',
-    output: {
-        file: 'dist/squirrelly.min.js',
-        format: 'umd',
-        name: 'Sqrl',
-        sourcemap: false
-    },
-    plugins: [
-        uglify()
-    ]
+  input: 'src/index.js',
+  output: {
+    file: 'dist/squirrelly.min.js',
+    format: 'umd',
+    name: 'Sqrl',
+    sourcemap: true
+  },
+  plugins: [
+    uglify()
+  ]
 },
 {
-    input: 'src/index.js',
-    output: {
-        file: 'dist/squirrelly.dev.js',
-        format: 'umd',
-        name: 'Sqrl',
-        sourcemap: true
-    },
-    plugins: [
-    ]
+  input: 'src/index.js',
+  output: {
+    file: 'dist/squirrelly.dev.js',
+    format: 'umd',
+    name: 'Sqrl',
+    sourcemap: true
+  },
+  plugins: [
+  ]
 },
 {
-    input: 'src/runtime.js',
-    output: {
-        file: 'dist/squirrelly.runtime.js',
-        format: 'umd',
-        name: 'Sqrl',
-        sourcemap: true
-    },
-    plugins: [
-        uglify()
-    ]
+  input: 'src/runtime.js',
+  output: {
+    file: 'dist/squirrelly.runtime.js',
+    format: 'umd',
+    name: 'Sqrl',
+    sourcemap: true
+  },
+  plugins: [
+    uglify()
+  ]
 },
 {
-    input: 'src/runtime.js',
-    output: {
-        file: 'dist/squirrelly.runtime.dev.js',
-        format: 'umd',
-        name: 'Sqrl',
-        sourcemap: true
-    },
-    plugins: [
-    ]
-}];
+  input: 'src/runtime.js',
+  output: {
+    file: 'dist/squirrelly.runtime.dev.js',
+    format: 'umd',
+    name: 'Sqrl',
+    sourcemap: true
+  },
+  plugins: [
+  ]
+}]
